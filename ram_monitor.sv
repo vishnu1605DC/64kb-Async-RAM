f2424a958f9ef7cacbbd8b6586fb9661ad181aad
K Vishnu Vardhan <vardhank1234@gmail.com>
2025-09-16 22:46:25 +0530

    Update ram_monitor.sv

--- ram_monitor.sv
+++ ram_monitor.sv
@@ -1,4 +1,3 @@
-
 `define MON_IF vif.monitor_mp
 
 class ram_monitor extends uvm_monitor;
@@ -31,15 +30,25 @@ class ram_monitor extends uvm_monitor;
       ram_sequence_item transaction;
       transaction = ram_sequence_item::type_id::create("transaction");
       
-      @(posedge `MON_IF.chip_en or posedge `MON_IF.rd_en or posedge `MON_IF.wr_en)begin
-      transaction.chip_en=`MON_IF.chip_en;
+      @(posedge `MON_IF.chip_en or posedge `MON_IF.rd_en or posedge `MON_IF.wr_en or posedge `MON_IF.reset)begin
+  
+        
+      
       transaction.wr_en=`MON_IF.wr_en;
       transaction.rd_en=`MON_IF.rd_en;
+      transaction.chip_en=`MON_IF.chip_en;
       transaction.addr=`MON_IF.addr;
       transaction.data_in=`MON_IF.data_in;
       transaction.data_out=`MON_IF.data_out;
-      transaction.reset=`MON_IF.reset;      
-      ap.write(transaction);
+      transaction.reset=`MON_IF.reset;  
+      #20;
+        
+        
+     ap.write(transaction);
+        
+       // $display("after write after delay At time %tns wr_en=%0b, rd_en=%0b, chip_en=%0b, addr=%0h, data_in=%0h,reset=%0b",$time,transaction.wr_en,transaction.rd_en,transaction.chip_en, transaction.addr, transaction.data_in, transaction.reset);
+                    
+        
       end
     end
       
